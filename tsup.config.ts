52ac42e48c024e7cf3d1937480d9904d8b651682
RJ Dellecese <rj@rjdellecese.com>
2024-12-19 15:27:40 +0000

    Fix tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,7 @@ export default defineConfig({
 	entry: ["index.ts"],
 	format: ["cjs", "esm"],
 	dts: true,
-	splitting: false,
 	sourcemap: true,
 	clean: true,
-	treeshake: true,
 	external: ["react", "convex"],
 });
